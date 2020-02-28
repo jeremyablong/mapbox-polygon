d5a9c2d0acb83dff854d8159740f335229f61b85
Theo Khayat <theokhayat23@gmail.com>
2020-02-28 16:39:34 -0600

    cors

--- src/apis/listings.js
+++ src/apis/listings.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 // Base URL for API requests for details of home listings
 
 export default axios.create({
-    baseURL: 'https://ftlsugcyt6.execute-api.us-east-2.amazonaws.com/dev',
+    baseURL: 'https://cors-anywhere.herokuapp.com/http://18.216.141.175:3000/database/mls',
 });
